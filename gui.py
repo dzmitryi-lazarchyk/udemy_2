14d04fc415a546e859474ceac237c162752d3ef2
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2023-02-11 17:53:24 +0300

    Implemented the Edit button.

--- gui.py
+++ gui.py
@@ -1,29 +1,56 @@
 from functions import *
 import PySimpleGUI as sg
 
+sg.theme('Dark')
+
 label = sg.Text("Type in a todo")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
+input_box = sg.InputText(tooltip="Enter todo", size=60,
+                         key="todo", default_text='')
 add_button = sg.Button("Add")
+
+list_box = sg.Listbox(values=get_todos(), key='todos',
+                      enable_events=True, size=(60, 10))
+edit_button = sg.Button("Edit")
+
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My todo App',
-                   layout=[[label, input_box, add_button],
-                           [exit_button]
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button],
+                           [exit_button],
                           ],
                    font=('Helvetica', 10)
                    )
 
+
 while True:
 
     event, values = window.read()
-    print(event)
-    print(values)
+    print('Event:',event)
+    print('Values:',values)
     match event:
         case 'Add':
             todos = get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             store_todos(todos)
+            input_box.update(value='')
+            list_box.update(values=get_todos())
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos =get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index]= new_todo
+            store_todos(todos)
+            list_box.update(values=todos)
+            input_box.update(value='')
+        case 'todos':
+            input_box.update(value=values['todos'][0].strip())
         case sg.WIN_CLOSED | "Exit":
             break
+
+
 event = window.close()
